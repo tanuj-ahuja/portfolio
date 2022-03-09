8e9768c96a867bce85feec18d91fd44a7d2904ca
Tanuj Ahuja <tanuj.ahuja@postman.com>
2022-03-09 11:25:36 -0500

    deployment setup

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    chainWebpack: config => {
-        config.module.rules.delete('eslint');
-    }
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/portfolio-template/'
+    : '/'
 }
